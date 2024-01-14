5bcd7166b7c6dae44380fe505bf18bdbb2fedd10
Frankdias92 <frankmcdias@gmail.com>
2024-01-13 23:40:09 -0300

    created detailHTML for a specific post

---
+++ api-guide/api-with-fetch/components/details.js
@@ -0,0 +1,22 @@
+// javascript for details Post
+const id = new URLSearchParams(window.location.search).get('id')
+const postItem = document.querySelector('#post-item')
+
+
+const renderDetails = async () => {
+  const url = "http://localhost:3000/posts"
+
+  const res = await fetch(`${url}/${id}`)
+  const post = await res.json()
+
+  const template = `
+    <h2>${post.title}</h2>
+    <p>${post.body.slice(0, 200)}</p>
+    <p><small>${post.likes} Likes</small></p>
+  `
+    
+  postItem.innerHTML = template
+  console.log(post)
+}
+
+  window.addEventListener('DOMContentLoaded', () => renderDetails())
--- api-guide/api-with-fetch/main.js
+++ api-guide/api-with-fetch/main.js
@@ -14,25 +14,14 @@ const renderPosts = async () => {
         <h2>${posts.title}</h2>
         <p><small>${posts.likes}</small></p>
         <p>${posts.body.slice(0, 200)}</p>
-        <a href="./components/details.html?id=${posts.id}">more...</a>
+        <a href="details.html?id=${posts.id}">more...</a>
       </div>
     `
   })
-
+  
+  
+  console.log(posts)
   container.innerHTML = renderTemplate
 }
-const name = 
 window.addEventListener('DOMContentLoaded', () => renderPosts())
 
-
-// javascript for details Post
-const id = new URLSearchParams(window.location.search).get('id')
-
-const renderDetails = async () => {
-  const res = await fetch("http://localhost:3000/posts" + id)
-  const post = await res.json()
-
-  console.log(post)
-}
-
-window.addEventListener('DOMContentLoaded', () => renderDetails())
