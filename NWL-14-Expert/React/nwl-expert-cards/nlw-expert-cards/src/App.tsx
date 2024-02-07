f85436439919e9d9f16f385ccaaa8849ee86807a
Frankdias92 <frankmcdias@gmail.com>
2024-02-07 01:48:14 -0300

    nlw 14 class React lesson 2

--- NWL-14-Expert/React/nwl-expert-cards/nlw-expert-cards/src/App.tsx
+++ NWL-14-Expert/React/nwl-expert-cards/nlw-expert-cards/src/App.tsx
@@ -21,12 +21,8 @@ export function App() {
     
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={{ date: new Date(), content: "Hello Dev's" }}/>
+      
       </div>
     </div>
   );
